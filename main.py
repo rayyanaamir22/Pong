29d03023689d3ad66487833b012433e137a34f60
rayyanaamir22 <92763211+rayyanaamir22@users.noreply.github.com>
2022-06-05 11:03:13 -0400

    Update main.py

--- main.py
+++ main.py
@@ -36,6 +36,9 @@ class Ball(pygame.sprite.Sprite): # Import pygame.sprite features
     self.image = pygame.Surface([10, 10]) # Square hitbox
     self.image.fill(white) # Ball colour
     self.rect = self.image.get_rect()
+    self.speed = 10
+    self.dx = 1 # Change in x
+    self.dy = 1 # Change in y
 
 # Create paddles
 paddle1 = Paddle()
@@ -45,6 +48,8 @@ paddle1.rect.y = 225
 paddle2 = Paddle()
 paddle2.rect.x = 715 # Initial coordinates
 paddle2.rect.y = 225
+
+paddleSpeed = 50 # Speed of moving up/down the screen
 
 # Create ball
 pong = Ball()
@@ -68,6 +73,22 @@ while not gameIsDone:
   for event in pygame.event.get():
     if event.type == pygame.QUIT: # Exit
       gameIsDone = True
+
+  key = pygame.key.get_pressed()
+  '''
+  The top-left corner is (0,0), so to move up, the y coordinates must decrement
+  '''
+  if key[pygame.K_w]: # Paddle1 UP
+    paddle1.rect.y -= paddleSpeed
+  if key[pygame.K_s]: # Paddle1 DOWN
+    paddle1.rect.y += paddleSpeed
+  if key[pygame.K_UP]: # Paddle2 UP
+    paddle2.rect.y -= paddleSpeed
+  if key[pygame.K_DOWN]: # Paddle2 DOWN
+    paddle2.rect.y += paddleSpeed
+
+  pong.rect.x += pong.speed
+  pong.rect.y += pong.speed
 
   redraw()
       
